cc6d56d48b6357cabba16d74cc09ac54940a4810
Manasvi Patel <W0835535@myscc.ca>
2024-09-17 22:24:18 -0400

    "Add jewels array with 6 Jewel objects in AppComponent"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Jewel } from './models/jewel.interface';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,55 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Manasvi-Patel-Learning-Angular';
-  name:string ='Manasvi';
-  age:number = 28;
+  name: string = 'Manasvi';
+  age: number = 28;
 
-
-
-
+  jewels: Jewel[] = [
+    {
+      id: 1,
+      name: "Diamond Ring",
+      type: "ring",
+      caratWeight: 1.5,
+      price: 500,
+      description: "A stunning diamond solitaire ring"
+    },
+    {
+      id: 2,
+      name: "Sapphire Necklace",
+      type: "necklace",
+      caratWeight: 2.0,
+      price: 3500,
+      description: "An elegant blue sapphire pendant"
+    },
+    {
+      id: 3,
+      name: "Emerald Earrings",
+      type: "earrings",
+      caratWeight: 1.0,
+      price: 2800
+    },
+    {
+      id: 4,
+      name: "Ruby Bracelet",
+      type: "bracelet",
+      caratWeight: 3.5,
+      price: 4200,
+      description: "A luxurious ruby-studded bracelet"
+    },
+    {
+      id: 5,
+      name: "Pearl Necklace",
+      type: "necklace",
+      caratWeight: 2.0,
+      price: 1500,
+      description: "A classic string of cultured pearls"
+    },
+    {
+      id: 6,
+      name: "Opal Ring",
+      type: "ring",
+      caratWeight: 0.75,
+      price: 1800
+    }
+  ];
 }
--- src/app/models/jewel.interface.ts
+++ src/app/models/jewel.interface.ts
@@ -9,3 +9,6 @@ export interface JewelInterface {
 
 
 }
+
+export class Jewel {
+}
