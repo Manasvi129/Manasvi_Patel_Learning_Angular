90dbd7585cf08fc850218a5d7ee8cbc219e996e5
Manasvi Patel <W0835535@myscc.ca>
2024-09-17 22:19:47 -0400

    "Created Jewel interface with five properties"

---
+++ src/app/models/jewel.interface.ts
@@ -0,0 +1,11 @@
+export interface JewelInterface {
+
+  id: number;               // Unique identifier for the jewel
+  name: string;            // Name of the jewel
+  type: string;            // Type of jewel (e.g., ring, necklace)
+  caratWeight: number;     // Weight of the jewel in carats
+  price: number;           // Price of the jewel
+  description?: string;    // Optional description of the jewel
+
+
+}
