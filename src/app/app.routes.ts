a916e8b7cc3291872644c16c610aae559201ff41
Manasvi Patel <W0835535@myscc.ca>
2024-11-01 11:01:08 -0400

    done with assignment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Home route
-  { path: 'modify', component: ModifyListItemComponent }, // Modify list item route
+  { path: 'modify', component: ModifyListItemComponent },
+  { path: 'modify/:id', component: ModifyListItemComponent },
   { path: '**', component: PageNotFoundComponent } // Wildcard route for 404 page
 ];
