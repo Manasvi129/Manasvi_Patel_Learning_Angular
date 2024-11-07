dc09c761234c045064eaeb71dd5dd118d6918882
Manasvi Patel <W0835535@myscc.ca>
2024-11-07 13:20:37 -0500

    Update DataService to use HttpClient for CRUD operations

--- src/app/data/mock-jewellery.ts
+++ src/app/data/mock-jewellery.ts
@@ -1,51 +1,40 @@
-
-
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { Jewellery } from '../models/jewel.interface';
 
-export const jewelleryItems: Jewellery[] = [
-  {
-    id: 1,
-    name: "Diamond Solitaire Ring",
-    type: "Ring",
-    caratWeight: 1.5,
-    price: 5999.10,
-    description: "A classic diamond solitaire set in white gold.",
-    imageUrl: "https://cdn.shopify.com/s/files/1/0081/7496/0736/t/5/assets/pf-2b3777ef-2ff8-442e-be4d-7cdf4e124d90--saltandpepperdiamondclawprongs.jpg?v=1576239493"
-  },
-  {
-    id: 2,
-    name: "Pearl Necklace",
-    type: "Necklace",
-    caratWeight: 0,
-    price: 2500.00,
-    description: "Elegant strand of cultured Akoya pearls.",
-    imageUrl: "https://i.etsystatic.com/5933460/r/il/b3ae5a/1900793620/il_fullxfull.1900793620_2o07.jpg"
-  },
-  {
-    id: 3,
-    name: "Sapphire Stud Earrings",
-    type: "Earrings",
-    caratWeight: 1.0,
-    price: 1200.00,
-    description: "Beautiful blue sapphire studs set in platinum.",
-    imageUrl: "https://i.ebayimg.com/images/g/CAoAAOSwwtdicho-/s-l1200.jpg"
-  },
-  {
-    id: 4,
-    name: "Gold Chain Bracelet",
-    type: "Bracelet",
-    caratWeight: 18,
-    price: 3500.00,
-    description: "18k gold chain bracelet with a lobster clasp.",
-    imageUrl: "https://i.ebayimg.com/00/s/MTYwMFgxNjAw/z/88MAAOSwHX9ha3ad/$_57.JPG?set_id=8800005007"
-  },
-  {
-    id: 5,
-    name: "Emerald Pendant",
-    type: "Pendant",
-    caratWeight: 2.5,
-    price: 7500.00,
-    description: "Stunning emerald pendant surrounded by diamonds, set in white gold.",
-    imageUrl: "https://www.mrporter.com/variants/images/1647597287204713/in/w2000_q60.jpg"
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private apiUrl = 'api/jewelleries'; // URL to web api
+
+  constructor(private http: HttpClient) { }
+
+  // GET all jewelleries
+  getJewelleries(): Observable<Jewellery[]> {
+    return this.http.get<Jewellery[]>(this.apiUrl);
   }
-];
+
+  // GET jewellery by id
+  getJewellery(id: number): Observable<Jewellery> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<Jewellery>(url);
+  }
+
+  // POST: add a new jewellery item
+  addJewellery(jewellery: Jewellery): Observable<Jewellery> {
+    return this.http.post<Jewellery>(this.apiUrl, jewellery);
+  }
+
+  // PUT: update an existing jewellery item
+  updateJewellery(jewellery: Jewellery): Observable<any> {
+    return this.http.put(this.apiUrl, jewellery);
+  }
+
+  // DELETE: delete a jewellery item
+  deleteJewellery(id: number): Observable<Jewellery> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<Jewellery>(url);
+  }
+}
---
+++ src/app/in-memory-data-service.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Jewellery } from './models/jewel.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class InMemoryDataService implements InMemoryDbService {
+  // returns an object with a jewelleries property,
+  // which is an array of Jewellery objects
+  createDb(): { jewelleries: Jewellery[] } {
+
+    const jewelleries: Jewellery[] = [
+      {
+        id: 1,
+        name: "Diamond Solitaire Ring",
+        type: "Ring",
+        caratWeight: 1.5,
+        price: 5999.10,
+        description: "A classic diamond solitaire set in white gold.",
+        imageUrl: "https://cdn.shopify.com/s/files/1/0081/7496/0736/t/5/assets/pf-2b3777ef-2ff8-442e-be4d-7cdf4e124d90--saltandpepperdiamondclawprongs.jpg?v=1576239493"
+      },
+      {
+        id: 2,
+        name: "Pearl Necklace",
+        type: "Necklace",
+        caratWeight: 0,
+        price: 2500.00,
+        description: "Elegant strand of cultured Akoya pearls.",
+        imageUrl: "https://i.etsystatic.com/5933460/r/il/b3ae5a/1900793620/il_fullxfull.1900793620_2o07.jpg"
+      },
+      {
+        id: 3,
+        name: "Sapphire Stud Earrings",
+        type: "Earrings",
+        caratWeight: 1.0,
+        price: 1200.00,
+        description: "Beautiful blue sapphire studs set in platinum.",
+        imageUrl: "https://i.ebayimg.com/images/g/CAoAAOSwwtdicho-/s-l1200.jpg"
+      },
+      {
+        id: 4,
+        name: "Gold Chain Bracelet",
+        type: "Bracelet",
+        caratWeight: 18,
+        price: 3500.00,
+        description: "18k gold chain bracelet with a lobster clasp.",
+        imageUrl: "https://i.ebayimg.com/00/s/MTYwMFgxNjAw/z/88MAAOSwHX9ha3ad/$_57.JPG?set_id=8800005007"
+      },
+      {
+        id: 5,
+        name: "Emerald Pendant",
+        type: "Pendant",
+        caratWeight: 2.5,
+        price: 7500.00,
+        description: "Stunning emerald pendant surrounded by diamonds, set in white gold.",
+        imageUrl: "https://www.mrporter.com/variants/images/1647597287204713/in/w2000_q60.jpg"
+      }
+    ];
+    return { jewelleries };
+  }
+  genId(jewelleries: Jewellery[]): number {
+    return jewelleries.length > 0 ? Math.max(...jewelleries.map(jewellery => jewellery.id)) + 1 : 11;
+  }
+}
